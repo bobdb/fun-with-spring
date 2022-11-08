1b020d713ada8a866951afd0332ebac5f5d0695a
bobdb <bobdb40@gmail.com>
2022-11-07 21:17:13 -0500

    create in mvc

--- src/main/java/net/bobdb/funwithspring/PersonController.java
+++ src/main/java/net/bobdb/funwithspring/PersonController.java
@@ -4,27 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class PersonController {
-
     @Autowired
     PersonService personService;
 
     @GetMapping("/person")
     public String findAll(Model model) {
-        List<Person> persons = personService.findAll().get();
+        List<Person> persons = personService.findAll().orElse(List.of());
+        Person find = new Person();
         model.addAttribute("persons", persons);
+        model.addAttribute("find", find);
         return "person";
     }
 
     @GetMapping(path = "/person/{id}")
     public String findById(@PathVariable Long id, Model model) {
-        Person person = personService.findById(id).get();
+        Person person = personService.findById(id).orElse(new Person());
         model.addAttribute("persons", List.of(person));
+        return "person";
+    }
+
+    @PostMapping(path = "/person")
+    public String create(@ModelAttribute Person find, Model model) {
+        personService.save(find.getName());
+        List<Person> persons = personService.findAll().orElse(List.of());
+        model.addAttribute("persons", persons);
+        model.addAttribute("find", find);
         return "person";
     }
 
